d20a18a5bdaf8bd6291b9a7e0bf9d9518e763918
MIRIEL Mathis <118454205+Elhexiar@users.noreply.github.com>
2025-01-25 12:12:10 +0100

    added gyro support

--- Assets/_INDIVIDUAL/_MIRIEL_M/PLAYER/S_playerBallManager.cs
+++ Assets/_INDIVIDUAL/_MIRIEL_M/PLAYER/S_playerBallManager.cs
@@ -12,16 +12,33 @@ public class S_playerBallManager : MonoBehaviour
     public float mouseXscale = 0.01f;
 
     public float mouseYscale = 0.01f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
+
+        // Check if the device supports the gyroscope
+        if (SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true; // Enable the gyroscope
+        }
+        else
+        {
+            Debug.LogWarning("Gyroscope not supported on this device.");
+        }
+
+        //Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         playerBallRigidbody.AddForce(playerBallGravity.normalized * playerBallGravityStrength);
+
 
         if(Input.GetAxis("Mouse X") != 0)
         {
@@ -31,6 +48,19 @@ public class S_playerBallManager : MonoBehaviour
         {
             playerBallGravity.z += Input.GetAxis("Mouse Y") * mouseYscale;
         }
+
+        // Get the gyroscope rotation rate
+        //Quaternion gyroRotation = Input.gyro.attitude;
+        Vector3 gyroRotation = Input.gyro.rotationRate;
+
+        print(gyroRotation);
+
+
+        // Use the gyroscope data to adjust the gravity vector
+        playerBallGravity.x += -gyroRotation.y * mouseXscale;
+        playerBallGravity.z += gyroRotation.x * mouseYscale;
         
+
+
     }
 }
